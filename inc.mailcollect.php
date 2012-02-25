0ddd22f9a0b962e2fdc080046b8abc874d0afa51
Jeremy Seago <seagoj@gmail.com>
2012-02-25 15:59:57 +0000

    "testing dbg::assert function"

--- inc.mailcollect.php
+++ inc.mailcollect.php
@@ -8,7 +8,9 @@ include_once('inc.controller.php');
 print "</head>\n<body>\n";
 
 $mailserver = "{".MAILHOST.":143/notls}INBOX";
-dbg::test($mailserver=="{mail.seagoj.com:143/notls}INBOX",__METHOD__);
+//dbg::test($mailserver=="{mail.seagoj.com:143/notls}INBOX",__METHOD__);
+dbg::test(true);
+dbg::test('true');
 $imap = imap_open($mailserver, EMAIL, PASS);
 //dbg::test(imap_errors()==NULL, __METHOD__);
 $emailCount = imap_num_msg($imap);
