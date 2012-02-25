d9bba21ceed76e89e110d8cd772b28e76a52669b
Jeremy Seago <seagoj@gmail.com>
2012-02-25 15:57:28 +0000

    "testing dbg::assert function"

--- lib.dbg/class.dbg.php
+++ lib.dbg/class.dbg.php
@@ -26,11 +26,18 @@ class dbg
         print "<span style='color:black;'>$dump</span></div>";
     }
     public function test($term, $method='', $fail='false') {
+        print "<div>PRINT</div>";
+        dbg::msg("message");
         assert_options(ASSERT_ACTIVE, true);
+        print "<div>ASSERT_ACTIVE</div>";
         assert_options(ASSERT_WARNING, true);
+        print "<div>ASSERT_WARNING</div>";
         assert_options(ASSERT_BAIL, false);
+        print "<div>ASSERT_BAIL</div>";
         assert_options(ASSERT_QUIET_EVAL, false);
+        print "<div>ASSERT_QUIET_EVAL</div>";
         assert_callback(ASSERT_CALLBACK, dbg::msg($term,$method, $fail));
+        print "<div>ASSERT_CALLBACK</div>";
 
         assert($term);
         print "ASSERTED";
