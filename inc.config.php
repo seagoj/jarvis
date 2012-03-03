09dd096fc9d43084de18f5ef7e6845a5b220cd42
Jeremy Seago <seagoj@gmail.com>
2012-03-03 23:11:11 +0000

    "update for testing"

--- inc.config.php
+++ inc.config.php
@@ -7,90 +7,28 @@
         $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
     }
     
-//    dbg::vardump($mysql_config);
     $db = $mysql_config['name'];
     $musicTbl = '`'.$db.'`.`music`';
     $configTbl = '`'.$db.'`.`config`';
-    
-    /*
-    $musicSQL = "CREATE TABLE  $musicTbl (
-    `index` INT NOT NULL AUTO_INCREMENT ,
-    `artist` VARCHAR( 30 ) NOT NULL ,
-    `album` VARCHAR( 50 ) NOT NULL ,
-    `format` VARCHAR( 10 ) NOT NULL ,
-    PRIMARY KEY (  `index` ) ,
-    INDEX (  `index` )
-    ) ENGINE = INNODB";
-    
-    
-    $configSQL = "CREATE TABLE $configTbl(
-    `index` INT NOT NULL AUTO_INCREMENT ,
-    `name` VARCHAR( 30 ) NOT NULL ,
-    `value` VARCHAR( 50 ) ,
-    PRIMARY KEY (  `index` ) ,
-    INDEX (  `index` )
-    ) ENGINE = INNODB";  
-    */
 
-$server = $mysql_config['host'].':'.$mysql_config['port'];
-print "<div>Server: ".$server."</div>";
-print "<div>User: ".$mysql_config['username']."</div>";
-print "<div>Pass: "."$mysql_config['password']."</div>";
-$conn = mysql_connect($server, $mysql_config['username'], $mysql_config['password']);
-if(!$conn) print "Connection failed";
-else print "Successful connection";
-print mysql_error();
+    $server = $mysql_config['host'].':'.$mysql_config['port'];
+    print "<div>Server: ".$server."</div>";
+    print "<div>User: ".$mysql_config['username']."</div>";
+    print "<div>Pass: "."$mysql_config['password']."</div>";
+    $conn = mysql_connect($server, $mysql_config['username'], $mysql_config['password']);
+    if(!$conn) print "Connection failed";
+    else print "Successful connection";
+    print mysql_error();
+
+
+    $sql = "SHOW TABLES FROM $db";
+    print "<div>$sql</div>";
+    $query = mysql_query($sql,$conn);
+    print mysql_error();
+    $result = mysql_fetch_assoc($query);
+    var_dump($result);
 
 /*
-$sql = "SHOW TABLES FROM $db";
-print "<div>$sql</div>";
-$query = mysql_query($sql,$conn);
-print mysql_error();
-$result = mysql_fetch_assoc($query);
-var_dump($result);
-*/
-
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('test','test')";
-print "<div>$sql</div>";
-$query = mysql_query($sql,$conn);
-print mysql_error();
-$result = mysql_fetch_assoc($query);
-var_dump($result);
-
-
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('movieTbl','movies')";
-print "<div>$sql</div>";
-$query = mysql_query($sql,$conn);
-print mysql_error();
-$result = mysql_fetch_assoc($query);
-var_dump($result);
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('bookTbl','books')";
-mysql_query($sql,$conn);
-print mysql_error();
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('gameTbl','games')";
-mysql_query($sql,$conn);
-print mysql_error();
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('mailHost','mail.seagoj.com')";
-mysql_query($sql,$conn);
-print mysql_error();
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('email','jarvis@seagoj.com')";
-mysql_query($sql,$conn);
-print mysql_error();
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('emailPass','gyte526yiojomp98h6rvy')";
-mysql_query($sql,$conn);
-print mysql_error();
-$sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('userTbl','users')";
-mysql_query($sql,$conn);
-print mysql_error();
-
-$sql = "SELECT * FROM $configTbl";
-print "<div>$sql</div>";
-$query = mysql_query($sql);
-$result = mysql_fetch_assoc($result);
-var_dump($result);
-print mysql_error();
-
-    /*
 	define(DBHOST,"localhost");
 	define(DBUSER,"icfjstor_library");
 	define(DBPASS,"NUEoilfsamdmaie23814");
