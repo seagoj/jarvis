52591f9a6f376af2fdfad8ea5f46e022a77f72e2
Jeremy Seago <seagoj@gmail.com>
2012-03-03 22:45:51 +0000

    "update for testing"

--- test.php
+++ test.php
@@ -2,46 +2,46 @@
 require_once('lib.dbg/class.dbg.php');
 require_once('lib.model/class.model.php');
     
-    $services = getenv("VCAP_SERVICES");
-    if($services) {
-        $services_json = json_decode($services,true);
-        $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
-    }
+$services = getenv("VCAP_SERVICES");
+if($services) {
+    $services_json = json_decode($services,true);
+    $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+}
     
-    dbg::vardump($mysql_config);
-    $db = $mysql_config['name'];
-    $musicTbl = '`'.$db.'`.`music`';
-    $configTbl = '`'.$db.'`.`config`';
+dbg::vardump($mysql_config);
+$db = $mysql_config['name'];
+$musicTbl = '`'.$db.'`.`music`';
+$configTbl = '`'.$db.'`.`config`';
     
-    $server = $mysql_config['host'].':'.$mysql_port['port'];
-    print $server;
-    $conn = mysql_connect($server, $mysql_config['username'], $mysql_config['password']);
-    if(!$conn) print "Connection failed";
-    else print "Successful connection";
-    print mysql_error();
+$server = $mysql_config['host'].':'.$mysql_port['port'];
+print $server;
+$conn = mysql_connect($server, $mysql_config['username'], $mysql_config['password']);
+if(!$conn) print "Connection failed";
+else print "Successful connection";
+print mysql_error();
     
-    /*** Connection Made */
-    $musicSQL = "CREATE TABLE  $musicTbl (
-    `index` INT NOT NULL AUTO_INCREMENT ,
-    `artist` VARCHAR( 30 ) NOT NULL ,
-    `album` VARCHAR( 50 ) NOT NULL ,
-    `format` VARCHAR( 10 ) NOT NULL ,
-    PRIMARY KEY (  `index` ) ,
-    INDEX (  `index` )
-    ) ENGINE = INNODB";
+/*** Connection Made ***/
+$musicSQL = "CREATE TABLE  $musicTbl (
+`index` INT NOT NULL AUTO_INCREMENT ,
+`artist` VARCHAR( 30 ) NOT NULL ,
+`album` VARCHAR( 50 ) NOT NULL ,
+`format` VARCHAR( 10 ) NOT NULL ,
+PRIMARY KEY (  `index` ) ,
+INDEX (  `index` )
+) ENGINE = INNODB";
     
-    print "<div>$musicSQL</div>";
-    $query = mysql_query($musicSQL,$conn);
-    print mysql_error();
-    $result = mysql_fetch_assoc($query);
-    var_dump($result);
+print "<div>$musicSQL</div>";
+$query = mysql_query($musicSQL,$conn);
+print mysql_error();
+$result = mysql_fetch_assoc($query);
+var_dump($result);
 
-    $sql = "SHOW TABLES;";
-    print "<div>$sql</div>";
-    $query = mysql_query($sql,$conn);
-    print mysql_error();
-    $result = mysql_fetch_assoc($query);
-    var_dump($result);
+$sql = "SHOW TABLES;";
+print "<div>$sql</div>";
+$query = mysql_query($sql,$conn);
+print mysql_error();
+$result = mysql_fetch_assoc($query);
+var_dump($result);
 
 /*
 $sql = "INSERT INTO $configTbl (`name`,`value`) VALUES ('test','test')";
