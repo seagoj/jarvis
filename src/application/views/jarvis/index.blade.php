f928800de25c67bdcd628b8dcad38fe02203aea0
Jeremy Seago <seagoj@gmail.com>
2012-09-05 14:46:02 -0500

    Initial Commit

---
+++ src/application/views/jarvis/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Jarvis</title>
+    <meta name="viewport" content="width=device-width">
+    {{ HTML::style('laravel/css/style.css') }}
+</head>
+<body>
+    <div class="wrapper">
+        Jarvis wrapper
+    </div>
+</body>
+</html>
--- src/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: http://www.seagoj.com/jarvis/inc.mailcollect.php');
-?>
